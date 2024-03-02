d66bdb82322d1c5b101d19b715dd390963fa6d7a
Bruno Rodrigues <brunorodrigues361@gmail.com>
2024-03-02 18:01:48 -0300

    Tabuada em python

---
+++ tabuada.py
@@ -0,0 +1,5 @@
+n1 = int(input("Informe o número a ser multiplicado: "))
+
+for n2 in range(1, 11):
+    multiplicacao = n1 * n2
+    print(f'O resultado da multiplicação é: {n1} x {n2} = {multiplicacao}')
